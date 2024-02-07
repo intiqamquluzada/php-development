152db703615e11cd3a0c17e22360fee3462492ba
intiqamquluzada <quluzadintiqam@gmail.com>
2024-02-07 11:07:45 +0400

    first-commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+////echo 'salam';
+//
+//$my_name = 'Intigam';
+//$my_age = 25;
+//$my_2_age = '26';
+//echo "$my_age , $my_2_age";
+
+//define("_name", "intigam");
+//echo _name;
+//echo "</br> is my friend";
+
+
+
+//$my_value = 'Intigam';
+//$my_number = 557;
+//echo $my_value[6];
+//
+//echo "</br>";
+//print $my_number %3 ;
+
+
+//$my_value = 453;
+//
+//$my_value %= 2;
+//
+//echo $my_value;
+
+
+//$my_name = 'Intigam';
+//$my_name .= ' Guluzade';
+//
+//echo $my_name;
+
+
+//$number = 222;
+//
+//echo $number++;
+//++$number;
+//
+//echo $number;
+
+
+//$num_1 = 34;
+//$num_2 = 43;
+//
+//echo $num_2 < $num_1;
